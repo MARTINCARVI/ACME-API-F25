1803d35356d7701143d0c37d77ba87fb501e589d
Martin CARVI <martincarvi@gmail.com>
2022-07-07 14:19:20 +0200

    Starter project

---
+++ users/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+
+
+class User:
+   """ dj-rest-auth
+django-debug-toolbar
+django-extensions
+django-filter
+djangorestframework"""
+   pass
